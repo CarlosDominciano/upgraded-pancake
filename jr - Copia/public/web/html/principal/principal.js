c07eb9d72071a164231a27d825bcb4069e7d3d1e
Carlos Dominciano <carlos.dominciano@bandtec.com.br>
2023-08-30 18:44:50 -0300

    Delete copy

--- jr - Copia/public/web/html/principal/principal.js
+++
@@ -1,2 +0,0 @@
-const divbanco = document.getElementById("div_banco")
-divbanco.textContent = sessionStorage.NOME_USUARIO;
--- jr - Copia/public/web/js/variaveis.js
+++
@@ -1,2 +0,0 @@
-//let pontos = pegardobanco, mandar pra session, trazer pra ca;
-let pontos = sessionStorage.PONTOS;
